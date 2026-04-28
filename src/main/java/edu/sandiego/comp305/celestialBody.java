d1a1a20c84cc6297b84f2b2df21f3dad82d2d374
Tammy Mathews <tmathews@sandiego.edu>
2026-04-27 21:28:56 -0700

    added CelestialBody interface stub

---
+++ src/main/java/edu/sandiego/comp305/celestialBody.java
@@ -0,0 +1,8 @@
+package edu.sandiego.comp305;
+
+public interface celestialBody {
+    //Vector2D getPosition();
+    double getMass();
+    //void applyForce(Vector2D force);
+    void update(double dt);
+}
