586f324af94ab9491b95dc1e670510f1966bb7d3
Tammy Mathews <tmathews@sandiego.edu>
2026-04-27 21:40:42 -0700

    feat: added stubs and variables to abstractBody

---
+++ src/main/java/edu/sandiego/comp305/abstractBody.java
@@ -0,0 +1,8 @@
+package edu.sandiego.comp305;
+
+public abstract class AbstractBody implements CelestialBody {
+    protected String name;
+    protected double mass;
+    //protected Vector2D position;
+    //protected Vector2D velocity;
+}
