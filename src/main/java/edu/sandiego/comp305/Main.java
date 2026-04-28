be4842e3ea33461d9da71d0f0900c583edbe4ad2
Devon Eder <deder@sandiego.edu>
2026-04-27 23:45:03 -0700

    FEAT: Solar System Factory Test Class Stubs

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -1,4 +1,4 @@
-package edu.sandiego.comp305;
+git branpackage edu.sandiego.comp305;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
